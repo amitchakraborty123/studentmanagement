1b4c301878db58503ba99dd35995cd6e1d0c4d5f
Amit Chakraborty <akchakraborty55@gmail.com>
2022-10-07 23:29:23 +0600

    Fix some files for uploade heroku

--- env/Scripts/activate.bat
+++ env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=F:\MSC\WebEngi\StudentManagementSystem-main_2\StudentManagementSystem-main\env
+set VIRTUAL_ENV=D:\My Files\#Varsity\MSc\1st\Web Engineering\practise\assignment_6\env
 
 if not defined PROMPT set PROMPT=$P$G
 
